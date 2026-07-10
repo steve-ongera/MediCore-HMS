7b2f2c454ab895937ec16b137b0e2789d6bb400d
steve-ongera <gadafisteve001@gmail.com>
2026-07-10 08:51:39 +0300

    made updates

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
-    name = 'api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "api"
+    verbose_name = "Hospital Management Information System"
+
+    def ready(self):
+        import api.signals  # noqa: F401
